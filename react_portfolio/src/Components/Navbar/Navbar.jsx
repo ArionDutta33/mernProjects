689219fbad420f32f9225fff86600e5ec006413c
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-07-15 16:22:45 +0530

    Added links to twitter and github

--- react_portfolio/src/Components/Navbar/Navbar.jsx
+++ react_portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,36 +1,14 @@
-import { useState } from "react";
-import { IoIosMenu } from "react-icons/io";
-
+import Button from "../Button/Button";
 const Navbar = () => {
-  const [isClicked, setIsClicked] = useState(true);
-
-  const handleMenuClick = () => {
-    setIsClicked(!isClicked);
-  };
-
   return (
     <>
-      {isClicked && (
-        <div className="menu-dropdown bg-blue-9500000 px-4 py-10  h-screen">
-          <div className="menu" onClick={handleMenuClick}>
-            <IoIosMenu fontSize={26} />
-          </div>
-          <ul className="mt-8 h-full flex flex-col gap-10">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-      )}
       <div>
         <nav className="flex px-4 py-6 justify-between">
           <div className="text-2xl">
             <span className="text-green-500">A</span>rion{" "}
             <span className="text-green-400">D</span>utta
           </div>
-          <div className="menu" onClick={handleMenuClick}>
-            <IoIosMenu fontSize={26} />
-          </div>
+          <Button text="Twitter" />
         </nav>
       </div>
     </>
