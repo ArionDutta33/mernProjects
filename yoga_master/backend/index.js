167063ad726f0696251018491299b52454a185c0
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-07-23 01:06:41 +0530

    cart routes+stripe integration

--- yoga_master/backend/index.js
+++ yoga_master/backend/index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 3000
 
 app.use(cors())
@@ -115,12 +117,93 @@ async function run() {
             const result = await classesCollection.updateOne(filter, updateDoc, options)
             res.send(result)
         })
+
+        //cart routes
+        app.post("/add-to-cart", async (req, res) => {
+            const newCartItem = req.body;
+            const result = await cartCollection.insertOne(newCartItem)
+            res.send(result)
+        })
+
+        //get cart item by id
+        app.get("/cart-item/:id", async (req, res) => {
+            const id = req.params.id;
+            const email = req.body.email;
+            const query = {
+                classId: id,
+                userMail: email
+            }
+            const projection = { classId: 1 }
+            const result = await cartCollection.findOne(query, { projection: projection });
+            res.send(result)
+        })
+        //cart-info by user email
+        app.get("/cart/:email", async (req, res) => {
+            const email = req.params.email
+            const query = { userMail: email }
+            const projection = { classId: 1 }
+            const carts = await cartCollection.find(query, { projection: projection })
+            const classIds = carts.map((cart) => new ObjectId(cart.classId))
+            const query2 = { _id: { $in: classIds } }
+            const result = await cartCollection.find(query2).toArray();
+            res.send(result)
+        })
+        //delete cart item
+        app.delete("/delete-cart-item/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { classId: id }
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+
+        })
+
+        //payments routes
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price) * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"]
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            })
+        })
+
+        //post payment info to db
+        app.post("/payment-info", async (req, res) => {
+            const paymentInfo = req.body;
+            const classesId = paymentInfo.classId;
+            const userEmail = paymentInfo.userEmail;
+            const singleClassId = req.query.classId;//!single or signle
+            let query;
+            if (singleClassId) {
+                query = { classesId: singleClassId, userEmail: userEmail }
+            } else {
+                query = { classId: { $in: classesId } }
+            }
+            const classesQuery = { _id: { $in: classesId.map(id => new ObjectId(id)) } }
+            const classes = await classesCollection.find(classesQuery.toArray())
+            const newEnrolledData = {
+                userEmail: userEmail,
+                classesId: singleClassId.map(id => new ObjectId(id)),
+                transactionId: paymentInfo.transactionId
+            }
+            const updatedDoc = {
+                $set: {
+                    totalEnrolled: classes.reduce((total, current) => total + current.totalEnrolled, 0) + 1 || 0,
+                    availableSeats: classes.reduce((total, current) => total + currnt.availableSeats, 0) - 1 || 0
+                }
+            }
+            const updatedResult = await classesCollection.updateMany
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
+        // await client.close();//!check this is closed
     }
 }
 run().catch(console.dir);
