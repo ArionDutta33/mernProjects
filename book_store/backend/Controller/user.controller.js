74877986e063a5105b077322b1fb0bcf33dd9df6
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-07-24 22:49:04 +0530

    Sign in and Login implemented

--- book_store/backend/Controller/user.controller.js
+++ book_store/backend/Controller/user.controller.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import User from "../Model/user.model.js"
 
 import bcryptjs from "bcryptjs"
 export const signup=async(req,res)=>{
     try {
         const{fullname,email,password}=req.body
-        const user=User.findOne({email})
+        const user=await User.findOne({email})
         if(user){
-            return res.status(400).json({message:"User already exists"});
+             return res.status(400).json({message:"User already exists"});
         }
-        const hashPassword=bcryptjs.hash(password,12)
+        const hashPassword=await bcryptjs.hash(password,12)
         const createdUser=new User({
             fullname:fullname,
             email:email,
@@ -24,11 +24,23 @@ export const signup=async(req,res)=>{
     }
 }
 
-// export const login=async(req,res)=>{
-//     try {
-//         const{email,password}=req.body
-//         const isMatch=bcryptjs.compare(password,usepassword)
-//     } catch (error) {
-        
-//     }
-// }
+export const login=async(req,res)=>{
+    try {
+        const{email,password}=req.body
+        const user=await User.findOne({email})
+        const isMatch=await bcryptjs.compare(password,user.password)
+        if(!user||!isMatch) {
+            return res.status(400).json({message:"Invalid password"})
+        }else{
+            res.status(200).json({message:"Login successful",user:{
+                _id:user._id,
+                fullname:user.fullname,
+                email:user.email,
+            }})
+        }
+                
+    } catch (error) {
+        console.log("error",error)
+        res.status(500).json({message:"internal server error"})
+    }
+}
