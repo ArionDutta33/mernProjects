7d957bb1cd1cbf56f43d5eb2efe900e9a20d9e51
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-07-26 21:00:49 +0530

    Added the login page

--- booking_app/client/src/App.jsx
+++ booking_app/client/src/App.jsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./Pages/IndexPage";
+import LoginPage from "./Pages/LoginPage";
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 };
