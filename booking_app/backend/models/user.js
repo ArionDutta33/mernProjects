67eaa0d7e43fb625060728901e62e5ab92ea1cfb
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-07-27 13:28:13 +0530

    Added the new keyword

--- booking_app/backend/models/user.js
+++ booking_app/backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String
