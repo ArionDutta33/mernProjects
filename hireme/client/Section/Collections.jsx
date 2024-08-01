151fbe0859a0694ff41f45b3556f2c37cdf472e7
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-08-02 00:40:24 +0530

    added a button to hire

--- hireme/client/Section/Collections.jsx
+++ hireme/client/Section/Collections.jsx
@@ -12,7 +12,7 @@ const Collections = () => {
             marginTop: "1rem",
           }}
           className="object-center object-cover"
-          src="https://images.pexels.com/photos/27435844/pexels-photo-27435844/free-photo-of-a-statue-of-a-man-on-a-horse-in-the-middle-of-a-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          src="https://images.pexels.com/photos/1668928/pexels-photo-1668928.jpeg?auto=compress&cs=tinysrgb&w=300"
           alt=""
         />
         <img
@@ -36,7 +36,7 @@ const Collections = () => {
             objectFit: "cover",
           }}
           className="object-center object-cover"
-          src="https://images.pexels.com/photos/27435844/pexels-photo-27435844/free-photo-of-a-statue-of-a-man-on-a-horse-in-the-middle-of-a-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          src="https://images.pexels.com/photos/26731366/pexels-photo-26731366/free-photo-of-young-woman-in-a-black-dress-and-a-red-coat.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
           alt=""
         />
         <img
@@ -48,7 +48,7 @@ const Collections = () => {
             objectFit: "cover",
           }}
           className="object-center object-cover"
-          src="https://images.pexels.com/photos/27435844/pexels-photo-27435844/free-photo-of-a-statue-of-a-man-on-a-horse-in-the-middle-of-a-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          src="https://images.pexels.com/photos/1123972/pexels-photo-1123972.jpeg?auto=compress&cs=tinysrgb&w=300"
           alt=""
         />
         <img
@@ -60,19 +60,7 @@ const Collections = () => {
             objectFit: "cover",
           }}
           className="object-center object-cover"
-          src="https://images.pexels.com/photos/27435844/pexels-photo-27435844/free-photo-of-a-statue-of-a-man-on-a-horse-in-the-middle-of-a-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          alt=""
-        />
-        <img
-          style={{
-            height: "15rem",
-            margin: "0 2rem",
-            width: "90%",
-            marginTop: "1rem",
-            objectFit: "cover",
-          }}
-          className="object-center object-cover"
-          src="https://images.pexels.com/photos/27435844/pexels-photo-27435844/free-photo-of-a-statue-of-a-man-on-a-horse-in-the-middle-of-a-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          src="https://images.pexels.com/photos/1707820/pexels-photo-1707820.jpeg?auto=compress&cs=tinysrgb&w=300"
           alt=""
         />
       </div>
