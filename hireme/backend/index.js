ebe49ab83afc79c9be89b81e2d87158fd0536bd0
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-08-02 21:10:38 +0530

    mail sent!!!

--- hireme/backend/index.js
+++ hireme/backend/index.js
@@ -13,15 +13,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/hiremeDB").then(() => {
 
 app.use(cors());
 app.use(express.json());
-// //nodemailer configuration
-// const transporter = nodemailer.createTestAccount({
-//     service: "gmail",
-//     auth: {
-//         user: process.env.user_mail,
-//         pass: process.env.user_pass
-//     }
-// })
-// //end
+//nodemailer configuration
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.USER_MAIL,
+        pass: process.env.USER_PASS
+    }
+})
+//end
 
 app.get("/test", async (req, res) => {
     const test = await photographerModel.find({})
@@ -29,30 +29,30 @@ app.get("/test", async (req, res) => {
     res.status(200).json({ message: 'ok', data: test })
 })
 
-// //email set up
-// app.post("/emial-send", async (req, res) => {
-//     const { photographerId, subject, text } = req.body;
-//     const photographer = await photographerModel.findById(photographerId);
-//     const photgrapherEmail = photographer.email;
-//     const photographerName = photographer.firstname + photographer.lastname;
+//email set up
+app.post("/email-send", async (req, res) => {
+    const { photographerId, subject, text } = req.body;
+    const photographer = await photographerModel.findById(photographerId);
+    const photgrapherEmail = photographer.email;
+    const photographerName = photographer.firstname + photographer.lastname;
 
-//     const mailOptions = {
-//         from: process.env.user_mail,
-//         to: photgrapherEmail,
-//         subject: subject,
-//         text: text
-//     }
-//     try {
-//         await transporter.sendMail(mailOptions);
-//         res.status(200).json({ message: "Email sent successfully" })
-//     } catch (error) {
-//         res.status(500).json({ message: "Failed to send email", error })
-//     }
-// })
-
-app.post("/email-send", (req, res) => {
-    console.log(req.body)
+    const mailOptions = {
+        from: process.env.USER_MAIL,
+        to: photgrapherEmail,
+        subject: subject,
+        text: text
+    }
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: "Email sent successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Failed to send email", error })
+    }
 })
+
+// app.post("/email-send", (req, res) => {
+//     console.log(req.body)
+// })
 app.listen(3000, () => {
     console.log("server up")
 })
